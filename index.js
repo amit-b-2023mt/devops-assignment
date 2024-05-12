010d120a6c466d4e23ccc4e4d12b0a04564726d1
Amit Bhapkar <2023mt93179@wilp.bits-pilani.ac.in>
2024-05-12 21:56:30 +0530

    Modified to test the automated build

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const sortAscending = require("./sortAscending.js");
 const sortDescending = require("./sortDescending.js");
-const array = [10, 6, 50, 1];
+const array = [10, 6, 50];
 
 console.log(
-	"Ascending order of the numbers 10, 6, 50, 1 is"
+	"Ascending order of the numbers 10, 6, 50 is"
 );
 const arrOutputAscending = sortAscending(array)
 console.log("{",...arrOutputAscending,"}");
 
 console.log(
-	"Descending order of the numbers 10, 6, 50, 1 is"
+	"Descending order of the numbers 10, 6, 50 is"
 );
 const arrOutputDescending = sortDescending(array)
 console.log("{",...arrOutputDescending,"}");
